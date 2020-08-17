15f27024a4498acb94cc0057ed1f27b51923f92e
Jose Gaston <jmcbgaston@gmail.com>
2020-08-16 22:36:04 -0700

    Added W4D1

---
+++ W4D1/graphnodes_bfs.rb
@@ -0,0 +1,21 @@
+class GraphNode
+
+    def bfs(starting_node, target_value)
+
+    end
+
+end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+
+bfs(a, "b")
+bfs(a, "f")
