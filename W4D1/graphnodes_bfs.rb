561dbc3f9b732a851a684721b8a54eeb842e2f71
Jose Gaston <jmcbgaston@gmail.com>
2020-08-16 23:25:06 -0700

    Completed graphnode bfs

--- W4D1/graphnodes_bfs.rb
+++ W4D1/graphnodes_bfs.rb
@@ -1,7 +1,30 @@
+require 'set'
+
 class GraphNode
+    attr_accessor :val, :neighbors
 
-    def bfs(starting_node, target_value)
+    def initialize(val)
+        self.val = val
+        self.neighbors = []
+    end
 
+    def depth_first(node, visited = Set.new())
+        # if this node has already been visited, then return early
+        return nil if (visited.include?(node.val))
+
+        # otherwise it hasn't yet been visited,
+        # so print its val and mark it as visited.
+        puts node.val
+        visited.add(node.val)
+
+        # then explore each of its neighbors
+        node.neighbors.each do |neighbor|
+            depth_first(neighbor, visited);
+        end
+    end
+
+    def add_neighbor(node)
+        self.neighbors << node
     end
 
 end
@@ -17,5 +40,45 @@ c.neighbors = [b, d]
 e.neighbors = [a]
 f.neighbors = [e]
 
-bfs(a, "b")
-bfs(a, "f")
+# a.depth_first(f)
+
+def depth_first(node, graph, visited = Set.new())
+    return nil if visited.include?(node.to_sym)
+    
+    puts node
+    visited.add(node.to_sym)
+    
+    graph[node.to_sym].each do |neighbor|
+        depth_first(neighbor, graph, visited);
+    end
+end
+
+graph = {
+    'a': ['b', 'c', 'e'],
+    'b': [],
+    'c': ['b', 'd'],
+    'd': [],
+    'e': ['a'],
+    'f': ['e']
+}
+
+# depth_first('f', graph)
+
+def bfs(starting_node, target_value)
+    queue = [starting_node]
+    visited = Set.new()
+
+    until queue.empty?
+        node = queue.shift
+        unless visited.include?(node)
+        return node.val if node.val == target_value
+        visited.add(node)
+        queue += node.neighbors
+        end
+    end
+    
+    return nil
+end
+
+p bfs(a, "b")
+p bfs(a, "f")
